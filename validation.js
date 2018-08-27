d410a7cca645d0113d29fe33e4804c981f18e663
Jason Wuertz <jwuertz@unb.ca>
2018-08-27 18:27:04 -0300

    add script file

---
+++ validation.js
@@ -0,0 +1,8 @@
+load( 'participant_validation.js' )
+load( 'trial_validation' )
+load( 'projection' )
+db = connect( "mongodb+srv://admin:4AnB1q3z8m3vZFOV@cluster0-lnoo1.mongodb.net/redline?retryWrites=true" )
+
+db.cumulative_entries.aggregate( pValid )
+db.cumulative_entries.aggregate( tValid )
+db.cumulative_entries.aggregate( projection )
