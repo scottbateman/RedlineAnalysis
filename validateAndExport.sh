38f1b5c8eb19854e17e92dfa0bc03b5c88f97847
Jason Wuertz <jwuertz@unb.ca>
2018-08-27 20:07:16 -0300

    add shell script to run this stuff

---
+++ validateAndExport.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+FIELDS="id, bar, trial, set, level, time, remainingHP, damageTaken, score, flamesInProximity, averageIntensityOfFlamesInProximity, activeFlames, fps, type, isValid, scoreRate"
+
+mongo "mongodb+srv://cluster0-lnoo1.mongodb.net/test" --username admin --password 4AnB1q3z8m3vZFOV validation.js
+
+mongoexport --host Cluster0-shard-0/cluster0-shard-00-00-lnoo1.mongodb.net:27017,cluster0-shard-00-01-lnoo1.mongodb.net:27017,cluster0-shard-00-02-lnoo1.mongodb.net:27017 --ssl --username admin --password 4AnB1q3z8m3vZFOV --authenticationDatabase admin --db redline --collection trial_validation --type csv --out trial_validation.csv --fields $FIELDS
+
+mongoexport --host Cluster0-shard-0/cluster0-shard-00-00-lnoo1.mongodb.net:27017,cluster0-shard-00-01-lnoo1.mongodb.net:27017,cluster0-shard-00-02-lnoo1.mongodb.net:27017 --ssl --username admin --password 4AnB1q3z8m3vZFOV --authenticationDatabase admin --db redline --collection atomic_validation --type csv --out atomic_validation.csv --fields $FIELDS
